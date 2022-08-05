afd4d7a9c5b5b842ee6e60fb278943184f4a1f5b
hdbrazier <104693569+hdbrazier@users.noreply.github.com>
2022-08-05 12:49:12 -0400

    First commit
    Questions 1-5 Python

---
+++ prework_assignments.py
@@ -0,0 +1,4 @@
+# qustion 1
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+def hello_name(user_name):
+    
